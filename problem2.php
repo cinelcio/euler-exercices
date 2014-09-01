81f3ce17f0cd1ae5288e69f1d75f4e357f11dd6d
Elcio de Faria Ribeiro <midelvis@gmail.com>
2014-09-01 00:06:00 -0300

    Final version of Problem2.php
    Finally finished. With the help of Gisele.

--- problem2.php
+++ problem2.php
@@ -11,31 +11,36 @@ Calculate the higher Fibonacci number until it
 reaches the parameter set
 */
 
-	function fibo($end, $printSequence) {
+	function fibo($endFibo, $printSequence) {
+		$a0 = 0;
+		$a1 = 1;
+		$a2 = 0;
+		 do {
+		 	$a2 = $a1 + $a0;
+			$a0 = $a1;
+			$a1 = $a2;
+			if($printSequence === true) {echo $a0."<br>";}
+		 } while ($a2 <= $endFibo);
+	}
+
+	function result($end) {
 		$n0 = 0;
 		$n1 = 1;
 		$n2 = 0;
+		$sum = 0;
 		 do {
 		 	$n2 = $n1 + $n0;
 			$n0 = $n1;
 			$n1 = $n2;
-			if($printSequence === true) {echo $n0."<br>";}
-		 } while ($n2 <= $end);
-		return $n0;
-	}
-//checks if a number is even
-	function isEven($num) {
-		if ($num % 2 != 0) {
-			return true;
-		}
+			if ($n0 % 2 == 1) {
+				$sum = $sum + $n0;
+			}
+		 } while ($n1 <= $end);
+			echo "Soma ".$sum;
 	}
 
-	function result($count) {
-		
-	}
-	//result(8);
-	fibo(10,false);
-
+		fibo (4000000,true);
+		result (4000000);
  ?>
 	<h1></h1>
 	</body>
